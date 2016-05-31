1383c5d3e4667b076a5ca53c1c8ab567b8104ecb
Durga Prasad <durgaprasad57@gmail.com>
2016-05-31 17:42:34 +0530

    changes to resource

---
+++ app/Http/Controllers/welcome.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController; 
+
+use Illuminate\Http\Request;
+
+use DB;
+use App\Http\Views;
+
+use Cache;
+class welcome extends BaseController {
+
+    /**
+     * The layout that should be used for responses.
+     */
+    protected $layout = 'layouts.master';
+
+    /**
+     * Show the user profile.
+     */
+    public function showProfile()
+    {
+        $this->layout->content = View::make('user.profile');
+		// return view('greeting', ['name' => 'James']);
+    }
+
+}
